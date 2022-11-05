3677ccb125762dbfdbe396dfdb9a2903d630b403
morronepablo <morronepablo@gmail.com>
2022-11-05 14:05:30 -0300

    subiendo proyecto 06-micelaneos

--- 06-micelaneos/src/app/app.module.ts
+++ 06-micelaneos/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent
   ],
   imports: [
     BrowserModule,
